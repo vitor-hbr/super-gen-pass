5eadcc9447304f172dec13dab24bc2829742b737
unknown <vitor.hbr@outlook.com>
2022-10-08 13:46:17 -0300

    feat: Created custom hook for password

--- components/password-generator.tsx
+++ components/password-generator.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { generate } from "supergenpass-lib";
 import {
   FaKey,
   FaEyeSlash,
@@ -11,63 +10,29 @@ import {
 import toast, { Toaster } from "react-hot-toast";
 
 import { Checkbox } from "./checkbox";
+import { usePasswordGenerator } from "../hooks/usePasswordGenerator";
 
 export const PasswordGenerator = () => {
-  const [masterPassword, setMasterPassword] = useState("");
-  const [url, setUrl] = useState("");
-  const [generatedPassword, setGeneratedPassword] = useState("");
-  const [onlyDomain, setOnlyDomain] = useState(true);
+  const {
+    masterPassword,
+    setMasterPassword,
+    hasGenerated,
+    setHasGenerated,
+    pairs,
+    setPairs,
+    onlyDomain,
+    setOnlyDomain,
+    generatedSize,
+    setGeneratedSize,
+    lastCopiedIndex,
+    maskPassword,
+    generatePasswords,
+    copyToClipboard,
+  } = usePasswordGenerator({});
   const [isMasterPasswordVisible, setIsMasterPasswordVisible] = useState(false);
   const [isGeneratedPasswordVisible, setIsGeneratedPasswordVisible] =
     useState(false);
-  const [copied, setCopied] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
-  const [generatedSize, setGeneratedSize] = useState(14);
-
-  const generatePassword = () => {
-    if (masterPassword && url) {
-      generate(masterPassword, url, { length: generatedSize }, (password) =>
-        setGeneratedPassword(password)
-      );
-      onCreatedPassword();
-    } else {
-      onMissingInput();
-    }
-    (document.activeElement as HTMLElement).blur();
-  };
-
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(generatedPassword);
-    setCopied(true);
-    onCopied();
-  };
-
-  const maskPassword = (password: string) => {
-    return password.replace(/./g, "*");
-  };
-
-  const onCreatedPassword = () =>
-    toast.success("Password created!", {
-      iconTheme: {
-        primary: "#7c3aed",
-        secondary: "#fff",
-      },
-    });
-
-  const onMissingInput = () =>
-    toast.error('Please, fill the "Master Password" and "Address" fields', {
-      iconTheme: {
-        primary: "#7c3aed",
-        secondary: "#fff",
-      },
-    });
-
-  const onCopied = () => toast("Copied to clipboard!", { icon: "📋" });
-
-  useEffect(() => {
-    setGeneratedPassword("");
-    setCopied(false);
-  }, [masterPassword, url, generatedSize, onlyDomain]);
 
   return (
     <main className="flex flex-col items-center justify-center bg-gradient-to-b from-violet-600 via-purple-900 text-white w-11/12 rounded-lg p-4 pb-12 max-w-sm md:my-auto select-none mb-[108px]">
@@ -104,15 +69,18 @@ export const PasswordGenerator = () => {
         <input
           type="text"
           placeholder="Enter the address of the site"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
+          value={pairs[0]?.url ?? ""}
+          onChange={(e) => {
+            setPairs([{ url: e.target.value, password: "" }]);
+            setHasGenerated(false);
+          }}
           className="text-sm w-full outline-none text-slate-900"
         />
       </span>
       <span className="flex drop-shadow-sm">
-        {!generatedPassword ? (
+        {!hasGenerated ? (
           <button
-            onClick={() => generatePassword()}
+            onClick={() => generatePasswords()}
             className="bg-white hover:bg-slate-900 p-3 rounded-lg text-violet-600 hover:text-white flex flex-row items-center justify-center transition-all"
           >
             <label className="pr-3 cursor-pointer">Generate Password</label>
@@ -126,14 +94,14 @@ export const PasswordGenerator = () => {
               className="bg-white hover:bg-slate-900 p-3 rounded-lg rounded-r-none text-violet-600 hover:text-white transition-all"
             >
               {isGeneratedPasswordVisible
-                ? generatedPassword
-                : maskPassword(generatedPassword)}
+                ? pairs[0]?.password
+                : maskPassword(pairs[0]?.password)}
             </button>
             <button
-              onClick={() => copyToClipboard()}
+              onClick={() => copyToClipboard(0)}
               className="bg-white hover:bg-slate-900 p-3 rounded-lg rounded-l-none text-violet-600 hover:text-white"
             >
-              {copied ? (
+              {lastCopiedIndex === 0 ? (
                 <FaClipboardCheck width={32} height={32} />
               ) : (
                 <FaClipboard width={32} height={32} />
---
+++ hooks/usePasswordGenerator.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { generate } from "supergenpass-lib";
+
+export type usePasswordGeneratorProps = {
+  missingInputMessage?: string;
+  copiedMessage?: string;
+  initialOnlyDomain?: boolean;
+  initialGeneratedSize?: number;
+};
+
+export type Pair = {
+  url: string;
+  password: string;
+};
+
+export const usePasswordGenerator = ({
+  missingInputMessage = "Please, fill the 'Master Password' and 'Address' fields",
+  copiedMessage = "Copied to clipboard!",
+  initialOnlyDomain,
+  initialGeneratedSize,
+}: usePasswordGeneratorProps) => {
+  const [masterPassword, setMasterPassword] = useState("");
+  const [pairs, setPairs] = useState<Pair[]>([]);
+  const [onlyDomain, setOnlyDomain] = useState(initialOnlyDomain ?? false);
+  const [generatedSize, setGeneratedSize] = useState(
+    initialGeneratedSize ?? 14
+  );
+  const [hasGenerated, setHasGenerated] = useState(false);
+  const [lastCopiedIndex, setLastCopiedIndex] = useState<number>(null);
+
+  const maskPassword = (password: string) => {
+    return password.replace(/./g, "*");
+  };
+
+  const generatePasswords = async () => {
+    if (masterPassword && pairs.length) {
+      const newPairs = [];
+      for (const pair of pairs) {
+        const password = await new Promise<string>((resolve) => {
+          generate(
+            masterPassword,
+            pair.url,
+            { length: generatedSize, onlyDomain },
+            (password) => {
+              console.log("Teste");
+              resolve(password);
+            }
+          );
+        });
+
+        newPairs.push({ ...pair, password: password });
+      }
+      setPairs(newPairs);
+      onCreatedPasswordToast();
+      setHasGenerated(true);
+    } else {
+      onMissingInputToast(missingInputMessage);
+    }
+    (document.activeElement as HTMLElement).blur();
+  };
+
+  const copyToClipboard = async (urlIndex: number) => {
+    navigator.clipboard.writeText(pairs[urlIndex].password);
+    setLastCopiedIndex(urlIndex);
+    onCopiedToast();
+  };
+
+  const onCreatedPasswordToast = () =>
+    toast.success("Password(s) created!", {
+      iconTheme: {
+        primary: "#7c3aed",
+        secondary: "#fff",
+      },
+    });
+
+  const onMissingInputToast = (message: string) =>
+    toast.error(message, {
+      iconTheme: {
+        primary: "#7c3aed",
+        secondary: "#fff",
+      },
+    });
+
+  const onCopiedToast = () => toast(copiedMessage, { icon: "📋" });
+
+  useEffect(() => {
+    setLastCopiedIndex(null);
+    setHasGenerated(false);
+  }, [masterPassword, generatedSize, onlyDomain]);
+
+  return {
+    masterPassword,
+    setMasterPassword,
+    pairs,
+    setPairs,
+    onlyDomain,
+    setOnlyDomain,
+    generatedSize,
+    setGeneratedSize,
+    lastCopiedIndex,
+    maskPassword,
+    generatePasswords,
+    copyToClipboard,
+    hasGenerated,
+    setHasGenerated,
+  };
+};
