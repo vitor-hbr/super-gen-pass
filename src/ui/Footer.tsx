0f390b134d022c02e08f9b548b04ee72a1a81848
unknown <vitor.hbr@outlook.com>
2023-06-14 23:51:08 -0300

    refactor: changed header and footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Footer({ avatar_url }: { avatar_url: string }) {
   return (
-    <footer className="text-sm md:text-base sticky bottom-0 flex w-full items-center justify-between gap-2 bg-gradient-to-t from-black p-6 text-white underline">
-      <a
+    <footer className="text-sm md:text-base sticky bottom-0 flex w-full items-center justify-between gap-2 bg-gradient-to-t from-black  px-8 py-4 text-white">
+      <Link
         href="https://github.com/vitor-hbr/super-gen-pass"
         target="_blank"
         rel="noopener noreferrer"
@@ -17,22 +18,26 @@ export function Footer({ avatar_url }: { avatar_url: string }) {
           className="rounded-full"
         />
         <span className="hidden pl-3 lg:block">Made by Vitor</span>
-      </a>
+      </Link>
 
       <nav className="flex gap-2 lg:gap-5">
-        <a href="/privacy-policy" target="_blank" rel="noopener noreferrer">
+        <Link href="/privacy-policy" target="_blank" rel="noopener noreferrer">
           Privacy Policy
-        </a>
-        <a href="/terms-of-service" target="_blank" rel="noopener noreferrer">
+        </Link>
+        <Link
+          href="/terms-of-service"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Terms of Service
-        </a>
-        <a
+        </Link>
+        <Link
           href="https://github.com/chriszarate/supergenpass-lib"
           target="_blank"
           rel="noopener noreferrer"
         >
           SuperGenPass Library
-        </a>
+        </Link>
       </nav>
     </footer>
   );
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -2,20 +2,51 @@
 
 import { useSession, signIn } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
   const { data } = useSession();
+  const pathname = usePathname();
+
   const userData = data?.user;
 
+  const links = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Stored Domains",
+      href: "/stored-domains",
+      requiresAuth: true,
+    },
+  ];
+
   return (
-    <header className="text-sm md:text-base sticky top-0 flex w-full items-center justify-end bg-gradient-to-b from-black p-6 text-white underline">
+    <header className="text-sm md:text-base sticky top-0 grid w-full grid-cols-[1fr_auto_1fr] items-center bg-gray-950 px-8 py-4 text-white">
+      <ul className="flex gap-6">
+        {links.map((link) => {
+          if (link.requiresAuth && !userData) return null;
+
+          const isActive = pathname === link.href;
+          return (
+            <li className={isActive ? "font-bold" : ""}>
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <strong className="animate-gradient-text bg-gradient-to-r from-violet-600 via-slate-300  to-purple-400 bg-clip-text font-display text-h5-desktop uppercase text-transparent duration-1000">
+        SuperGenPass
+      </strong>
       {userData && (
         <Image
           src={userData.image}
           alt="Profile Picture"
           width={56}
           height={56}
-          className="rounded-full"
+          className=" justify-self-end rounded-full"
         />
       )}
       {!userData && (
